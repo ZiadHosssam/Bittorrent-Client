97bd1e90575b49b02ce4fb7fe8f5157731424322
ZiadHosssam <ziad.h.ramadona@gmail.com>
2025-06-14 20:51:28 +0300

    add the build foler and the script to run the program

---
+++ your_program.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e 
+
+(
+  cd "$(dirname "$0")"
+  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+  cmake --build ./build
+)
+
+exec $(dirname $0)/build/bittorrent "$@"
